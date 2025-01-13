90010a55e62aa465d073e6ea97d517a1c5d961d4
zeeshan <160707874+zeeshan00013@users.noreply.github.com>
2025-01-13 15:12:41 +0500

    Update Navbar.jsdsa

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -12,7 +12,7 @@ export const Navbar = () => {
         <ul className="flex space-x-8">
           <Link to='/' className="text-white hover:text-gray-300">Home</Link>
           <Link to='/Cart' href="#" className="text-white hover:text-gray-300">Add product</Link >
-          <Link to='/Data' href="#" className="text-white hover:text-gray-300">data</Link >
+          <Link to='/Data' href="#" className="text-white hover:text-gray-500">data</Link >
           <Link to='/Images' href="#" className="text-white hover:text-gray-300">image</Link >
           <Link to='/Form' href="#" className="text-white hover:text-gray-300">form</Link >
           <Link to='/edit' href="#" className="text-white hover:text-gray-200">edit</Link >
