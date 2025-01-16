54dc3ba8c9268e7b199498a856351c3a87723a78
zeeshan <160707874+zeeshan00013@users.noreply.github.com>
2025-01-16 14:33:23 +0500

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -10,7 +10,7 @@ export const Navbar = () => {
       <div className="text-white font-bold text-2xl ">Logo</div>
 
         <ul className="flex space-x-8">d
-dsfsdf          <Link to='/Cart' href="#" className="text-white hover:text-weqeqasdadasgrawewy-800">Add product</Link >
+dsfsdf          <Link to='/Cart' href="#" className="text-white hover:text-wedsadasdqeqasdadasgrawewy-800">Add product</Link >
           <Link to='/Data' href="#" className="text-white hover:weqeqtexfsdfsdt-grqqweqwweeqweqqway-344">data</Link >
           <Link to='/Images' href="#" className="text-white howeveeqweqr:wqqweqetext-gray-400">image</Link >
           <Link to='/Form' href="#" className="text-fsdfsfwhite hover:sdsdfsdfsdfsdfsd-gray-200">form</Link >
