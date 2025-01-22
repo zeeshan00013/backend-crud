7894172fe74a1001fa1baaa00fe717cee06ddca5
zeeshan <160707874+zeeshan00013@users.noreply.github.com>
2025-01-22 13:00:12 +0500

    Update Edit.js

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-const Edit = ({ data, onSubmit, ondasdaadsancel }) => {
+const Edit = ({ data, onSubmit, ondasdaaddsasancel }) => {
     // Provide default values for formData if data is undefined
  const[name,setName]=useState("");
  const[email, setEmail] = useState('');
