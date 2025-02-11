b83971c9a4b272dec1df961b8975b210ef1d0bcc
zeeshan <160707874+zeeshan00013@users.noreply.github.com>
2025-02-11 11:42:56 +0500

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const Navbar = () => {
   return (
     <div>
-        <nav className="bg-gray-800 py-5 container dsad">
+        <nav className="bg-gray-800 py-5 container dsdasdasad">
 
       <div className=" space-x-6  flex justify-center">
       <div className="text-white font-bold text-dsadasdas ">Logo</div>
