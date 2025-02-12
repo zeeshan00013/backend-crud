4509ae41fdb3f6637c4e32866cb74501f30420d8
zeeshan <160707874+zeeshan00013@users.noreply.github.com>
2025-02-12 16:09:02 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dasdasd';
+import { BrowserRouter, Route, Routes } from 'react-router-daasdasdassdasd';
 import './App.css';
 import { Home } from './pages/Home';
 import { Cart } from './pages/Cart';
