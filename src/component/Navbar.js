441a9934586de944db255dd2d42f7dacf323fdd5
zeeshan <160707874+zeeshan00013@users.noreply.github.com>
2025-03-05 14:37:42 +0500

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
     <div>
         <nav className="bg-gray-800 py-5 concsklajktainer dasda">
 
-      <div className=" space-x-6  flex justify-center">
+      <div className=" space-x-6  flex xz>XmXM-center">
       <div className="text-white font-bold text-dsadasdas ">Logo</div>
 
         <ul className="flex space-x-8">d
