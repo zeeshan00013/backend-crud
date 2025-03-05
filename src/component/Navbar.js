5558adaccdc325106fb98296b80be46ed964654a
zeeshan <160707874+zeeshan00013@users.noreply.github.com>
2025-03-05 14:38:57 +0500

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
         <nav className="bg-gray-800 py-5 lsldsajldjk dasda">asdasdasd
 claskdlasdl
       <div className=" space-x-6  flexsdasdx xz>sdadasd-center">
-      <div className="text-white font-bold text-dsadasdas ">Logo</div>
+      <div className="text-white font-bold text-dsadasdaxXXZs ">Logo</div>
 
         <ul className="flex space-x-8">d
 dsfsdf          <Link to='/Cart' href="#" className="text-white hover:text-wedsadasdqeqasdadasgrawewy-800">Add product</Link >
