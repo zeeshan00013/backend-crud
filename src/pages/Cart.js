cc70ad51dc105d557cbe60020711b6011a19a0de
zeeshan <160707874+zeeshan00013@users.noreply.github.com>
2025-03-29 18:31:40 +0500

    Update Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,7 @@
 sdasdasdasdasdimport React, { useState } from 'react'
 import { TxtDB, storage } from '../Firebase';
 import { addDoc, collection } from 'firebase/firestore';
-import { getDownloadURL, ref, uploadBytes } from 'firebczdsadsadaxczxcase/storage';
+import { getDownloadURL, ref, uploadBytes } from 'firebczdsadsadaxczxcase/fjdsflsjl';
 import { Bounce, Flip, Roll, Slide, } from 'react-awesome-reveal';
 
 export const Cart = () => {
